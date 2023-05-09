8622c8b5b79249d6abd162b3d32572cd766b2e61
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2023-05-09 21:40:22 +0300

    Add clear button

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
-import React, {useState} from 'react';
-import {
-  AdaptivityProvider,
-  ConfigProvider,
-  AppRoot,
-  View
-} from '@vkontakte/vkui';
+import React, { useState } from 'react';
+import { AdaptivityProvider, ConfigProvider, AppRoot, View } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
-import {bookings as defaultBookings} from "./bookings";
-import BookingList from "./BookingList/BookingList";
-import Form from "./Form/Form";
+import { bookings as defaultBookings } from './bookings';
+import BookingList from './BookingList/BookingList';
+import Form from './Form/Form';
 
 function Main() {
   const [bookings, setBookings] = useState(defaultBookings);
-  const [panel, setPanel] = useState('form')
+  const [panel, setPanel] = useState('form');
   return (
-        <View activePanel={panel}>
-          <BookingList bookings={bookings} id={'bookings'} setPanel={setPanel}/>
-            <Form id={'form'}/>
-        </View>
+    <View activePanel={panel}>
+      <BookingList id={'bookings'} bookings={bookings} setPanel={setPanel} />
+      <Form id={'form'} bookings={bookings} setPanel={setPanel} setBookings={setBookings} />
+    </View>
   );
 }
 
 function App() {
   return (
-    <ConfigProvider appearance={'light'}>
+    <ConfigProvider appearance={'light'} transitionMotionEnabled={false}>
       <AdaptivityProvider>
         <AppRoot>
           <Main />
--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,79 +1,160 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-    Button,
-    DatePicker,
-    FormItem,
-    FormLayout,
-    FormLayoutGroup,
-    Group, Header, Input,
-    Panel,
-    PanelHeader,
-    SegmentedControl,
-    Select, Textarea
-} from "@vkontakte/vkui";
+  Button,
+  DateInput,
+  FormItem,
+  FormLayout,
+  FormLayoutGroup,
+  Group,
+  Header,
+  Input,
+  Panel,
+  PanelHeader,
+  SegmentedControl,
+  Select,
+  Textarea,
+} from '@vkontakte/vkui';
 import styles from './Form.module.css';
+import PropTypes from 'prop-types';
 
-function Form() {
-    const generateSelectableNumbers = (amount) => {
-      return [...Array(amount).keys()].map((elem) => {return {label: `${elem + 1}`, value: elem + 1}})
+function Form({ setPanel, setBookings, bookings }) {
+  const [tower, setTower] = useState('A');
+  const [floor, setFloor] = useState('');
+  const [room, setRoom] = useState('');
+  const [date, setDate] = useState(() => new Date());
+  const [fromTime, setFromTime] = useState('');
+  const [toTime, setToTime] = useState('');
+  const [comment, setComment] = useState('');
+  const [status, setStatus] = useState({
+    floor: 'default',
+    room: 'default',
+    fromTime: 'default',
+    toTime: 'default',
+    toTimeMessage: '',
+  });
+
+  const generateSelectableNumbers = (amount) => {
+    return [...Array(amount).keys()].map((elem) => {
+      return { label: `${elem + 1}`, value: `${elem + 1}` };
+    });
+  };
+
+  const sendForm = () => {
+    const newStatus = {
+      floor: floor !== '' ? 'default' : 'error',
+      room: room !== '' ? 'default' : 'error',
+      fromTime: fromTime !== '' ? 'default' : 'error',
+      toTime: toTime !== '' && fromTime < toTime ? 'default' : 'error',
+      toTimeMessage:
+        fromTime !== '' && toTime !== '' && fromTime >= toTime
+          ? 'Время окончания не может быть меньше времени начала.'
+          : '',
+    };
+
+    setStatus(newStatus);
+    if (Object.values(newStatus).some((e) => e === 'error')) {
+      return;
     }
 
-    const getCurrentDate = () => {
-      let today = new Date();
-      return {day: today.getDate(), month: today.getMonth() + 1, year: today.getFullYear()};
-    }
+    console.log(date);
+    const booking = {
+      tower,
+      floor,
+      room,
+      date: `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`,
+      fromTime,
+      toTime,
+      comment,
+    };
 
-    return (
-        <Panel>
-            <PanelHeader>Booking Service</PanelHeader>
-            <Group className={styles.group}  header={<Header>Бронирование переговорной</Header>}>
-                <FormLayout>
-                    <FormLayoutGroup mode={'horizontal'}>
-                        <FormItem top={'Башня'}>
-                            <SegmentedControl size={'m'} options={[{label: 'A', value:'A'}, {label: 'Б', value:'B'}]}/>
-                        </FormItem>
-                        <FormItem top={'Этаж'}>
-                            <Select placeholder={'Не выбран'} options={generateSelectableNumbers(27)}/>
-                        </FormItem>
-                        <FormItem top={'Комната'}>
-                            <Select placeholder={'Не выбран'} options={generateSelectableNumbers(10)}/>
-                        </FormItem>
-                    </FormLayoutGroup>
-                    <FormItem top={'дата'}>
-                        <DatePicker
-                            min={getCurrentDate()}
-                            defaultValue={getCurrentDate()}
-                            max={{day: 1, month: 1, year: 2050}}
-                            dayPlaceholder="ДД"
-                            monthPlaceholder="ММММ"
-                            yearPlaceholder="ГГГГ"
-                        />
-                    </FormItem>
-                    <FormLayoutGroup mode={'horizontal'}>
-                        <FormItem top={'От'}>
-                            <Input type={'time'}/>
-                        </FormItem>
-                        <FormItem top={'До'}>
-                            <Input type={'time'}/>
-                        </FormItem>
-                    </FormLayoutGroup>
-                    <FormLayoutGroup mode={'horizontal'}>
-                        <FormItem top={'Комментарий'}>
-                            <Textarea/>
-                        </FormItem>
-                    </FormLayoutGroup>
-                    <div className={styles.buttonWrapper}>
-                        <Button size={'l'} appearance={'neutral'}>
-                            Очистить
-                        </Button>
-                        <Button type={'submit'} size={'l'} >
-                            Отправить
-                        </Button>
-                    </div>
-                </FormLayout>
-            </Group>
-        </Panel>
-    )
+    console.log(booking);
+    setBookings([...bookings, booking]);
+    setPanel('bookings');
+  };
+
+  const clearForm = () => {
+    setTower('A');
+    setFloor('');
+    setRoom('');
+    setDate(() => new Date());
+    setFromTime('');
+    setToTime('');
+    setComment('');
+  };
+
+  return (
+    <Panel>
+      <PanelHeader>Booking Service</PanelHeader>
+      <Group className={styles.group} header={<Header>Бронирование переговорной</Header>}>
+        <FormLayout>
+          <FormLayoutGroup mode={'horizontal'}>
+            <FormItem top={'Башня'}>
+              <SegmentedControl
+                value={tower}
+                onChange={setTower}
+                size={'m'}
+                options={[
+                  { label: 'A', value: 'A' },
+                  { label: 'Б', value: 'B' },
+                ]}
+              />
+            </FormItem>
+            <FormItem top={'Этаж'} status={status.floor}>
+              <Select
+                value={floor}
+                onChange={(e) => setFloor(e.target.value)}
+                placeholder={'Не выбран'}
+                options={generateSelectableNumbers(27)}
+              />
+            </FormItem>
+            <FormItem top={'Комната'} status={status.room}>
+              <Select
+                value={room}
+                onChange={(e) => setRoom(e.target.value)}
+                placeholder={'Не выбран'}
+                options={generateSelectableNumbers(10)}
+              />
+            </FormItem>
+          </FormLayoutGroup>
+          <FormLayoutGroup mode={'horizontal'}>
+            <FormItem top={'дата'}>
+              <DateInput value={date} disablePast={true} onChange={setDate} />
+            </FormItem>
+            <FormItem top={'От'} status={status.fromTime}>
+              <Input value={fromTime} type={'time'} onChange={(e) => setFromTime(e.target.value)} />
+            </FormItem>
+            <span className={styles.intervalSymbol}>-</span>
+            <FormItem top={'До'} status={status.toTime} bottom={status.toTimeMessage}>
+              <Input value={toTime} type={'time'} onChange={(e) => setToTime(e.target.value)} />
+            </FormItem>
+          </FormLayoutGroup>
+          <FormLayoutGroup mode={'horizontal'}>
+            <FormItem top={'Комментарий'}>
+              <Textarea
+                placeholder={'Введите комментарий...'}
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              />
+            </FormItem>
+          </FormLayoutGroup>
+          <div className={styles.buttonWrapper}>
+            <Button size={'l'} appearance={'neutral'} onClick={clearForm}>
+              Очистить
+            </Button>
+            <Button type={'submit'} size={'l'} onClick={sendForm}>
+              Отправить
+            </Button>
+          </div>
+        </FormLayout>
+      </Group>
+    </Panel>
+  );
 }
+
+Form.propTypes = {
+  bookings: PropTypes.array,
+  setPanel: PropTypes.func,
+  setBookings: PropTypes.func,
+};
 
 export default Form;
